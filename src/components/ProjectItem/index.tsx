32e6fc8995d98183989310531e0805882dff4e81
Daniel Junio <danielju71@gmail.com>
2025-04-10 20:02:45 -0300

    ajuste animação hover projects

--- src/components/ProjectItem/index.tsx
+++ src/components/ProjectItem/index.tsx
@@ -1,4 +1,6 @@
 import Image, { StaticImageData } from "next/image";
+import Link from "next/link";
+import { useState } from "react";
 
 export interface IProjectItem {
   id: number,
@@ -6,18 +8,22 @@ export interface IProjectItem {
   description: string,
   image: StaticImageData,
 }
-const ProjectItem = ({ item }: { item: IProjectItem }) => {
-  return (
-    <li className="rounded-2xl overflow-hidden">
 
-      <div className="bg-[#363636] h-[200px] flex justify-center items-center">
-        <Image width={200} height={200} alt="project image" src={item.image.src} />
-      </div>
-      <div className="bg-[#272829] p-6 flex flex-col gap-2">
-        <h1 className="font-bold text-lg">{item.name}</h1>
-        <p className="text-sm">{item.description}</p>
-      </div>
-    </li>
+const ProjectItem = ({ item }: { item: IProjectItem }) => {
+
+  return (
+    <Link href={"/"} className="group">
+      <li className="rounded-2xl overflow-hidden">
+
+        <div className="bg-[#363636] h-[200px] flex justify-center items-center">
+          <Image width={140} height={140} alt="project image" src={item.image.src} className="transition-transform duration-300 group-hover:scale-110" />
+        </div>
+        <div className="bg-[#272829] p-6 flex flex-col gap-2">
+          <h1 className="font-bold text-lg group-hover:text-[#8e81ff] transition-transform duration-300">{item.name}</h1>
+          <p className="text-sm">{item.description}</p>
+        </div>
+      </li>
+    </Link>
   )
 }
 
